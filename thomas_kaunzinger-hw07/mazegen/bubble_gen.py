2af8f0cd4d6fa0b83f2a20ac8d23cc06e8a7e37f
tkaunzinger <tkaunzinger@comcast.net>
2020-10-30 07:08:13 -0400

    stub for mazegen

---
+++ thomas_kaunzinger-hw07/mazegen/bubble_gen.py
@@ -0,0 +1,21 @@
+# Using PCG_Gazebo, a library to enable Gazebo world generation within python
+# https://github.com/boschresearch/pcg_gazebo
+# pip3 install pcg-gazebo
+from pcg_gazebo.generators.creators import create_models_from_config
+from pcg_gazebo.task_manager import Server
+from pcg_gazebo.generators import WorldGenerator
+
+# Imports
+import random
+
+# Generates a "bubble" world with lots of bubble loops that a robot would get
+# stuck on if attempting to wall-follow
+
+
+
+
+if __name__ == '__main__':
+
+    pass
+
+
